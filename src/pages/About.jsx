4234f11c8cd3ace35b78fe3a283f867a8bb7112f
Brian <41770471+Brianleft28@users.noreply.github.com>
2024-07-11 21:18:16 -0300

    updates

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -1,5 +1,29 @@
+// import img
+import WomanImg from '../img/about/woman.png'
+// import link
+import { Link } from 'react-router-dom'
+
 const About = () => {
-  return <section className="section bg-yellow-200">About</section>;
+  return <section className="section">
+    <div className="container mx-auto h-full relative">
+      {/* text & img wrapper */}
+      <div className='flex flex-col lg:flex-row h-full items-center justify-center gap-x-0 text-center lg:text-left lg:pt-16'>
+        {/* iamge */}
+        <div className='lg:relative lg:left-32 flex-1 max-h-96 lg:max-h-max order-2 lg:order-none overflow-hidden'>
+          <img className='max-w-md' src={WomanImg} alt="" />
+        </div>
+        {/* text */}
+        <div className='flex-1 pt-36 pb-14 lg:pt-0 lg:w-auto z-10 flex-col justify-center items-center lg:items-start'>
+          <h1 className='h1'>About me</h1>
+          <p className='lg:mx-0 mx-auto'>Lorem ipsum <b>dolor sit</b> amet consectetur, adipisicing elit. Sed sit, dolore at unde nostrum non adipis Lorem ipsum dolor sit amet. <b>Dolor sit</b> Lorem ipsum dolor sit amet.</p>
+          <br />
+          <br />
+          <p className='mb-12'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Assumenda, doloremque?</p>
+          <Link to={'/portfolio'} className='btn max-w-sm mx-auto lg:mx-0'>View my work</Link>
+        </div>
+      </div>
+    </div>
+  </section>;
 };
 
 export default About;
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -5,26 +5,22 @@ const Home = () => {
   return (
     <section className="section">
       <div className="container mx-auto h-full relative">
-        {/* Text & img wrapper */}
+        {/* text and image wrapper */}
         <div className="flex flex-col justify-center">
           {/* text */}
-          <div className="w-full pt-36 pb-14 lg:pt-0 lg:pb-0 lg:w-auto z-10 lg:absolute flex flex-col justify-center items-center lg:items-start ">
-            <h1 className="h1">
-              photographer <br /> & film maker{" "}
-            </h1>
-            <p className="text-[26px] lg:text-[36px] font-primary mb-4 lg:mb-12">
-              Buenos Aires, Argentina
-            </p>
-            <Link className="btn mb-[30px]" to={"/contact"}>
-              Hire Me
-            </Link>
-          </div>
+        <div className="w-full pt-36 pb-14 lg:pt-0 lg:pb-0 lg:w-auto z-10 lg:absolute flex flex-col  justify-center items-center lg:items-start lg:top-36 lg:left-32">
+          <h1 className="h1">
+            photographer <br /> & film maker
+          </h1>
+          <p className="text-[26px] lg:text-[36px] font-primary mb-4 lg:mb-12">Buenos Aires, Argentina</p>
+          <Link className="btn mb-[30px]" to={'/contact'}>hire me</Link>
+        </div>
           {/* image */}
-          <div className="flex justify-end max-h-96 lg:max-h-max">
-            <div className="relative lg:-right-40 overflow-hidden">
-              <img src={WomanImg} alt="" />
-            </div>
+        <div className="flex justify-end max-h-96 lg:max-h-max">
+          <div className="relative lg:-right-16 overflow-hidden">
+            <img src={WomanImg} alt="" />
           </div>
+        </div>
         </div>
       </div>
     </section>
